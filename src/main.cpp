7b3bf416db66a0a3a4b57d80864a19e2e63bfc71
shahd-moh-abdel <shahd.mohammed.abdel@gmail.com>
2025-09-01 23:19:09 +0300

    working on colors

--- src/main.cpp
+++ src/main.cpp
@@ -38,29 +38,55 @@ GLFWwindow* windowSetUp() {
   return window;
 }
 
-
-int main()
-{
-  GLFWwindow* window = windowSetUp();
-
+void createVertexBuffer()
+{  
   GLfloat verts[] =
     {
-      +0.0f, +0.0f,
-      +1.0f, +1.0f,
-      -1.0f, +1.0f,
-
-      +0.0f, +0.0f,
-      -1.0f, -1.0f,
-      +1.0f, -1.0f,
+      // #1 triangle 0, 1, 2
+      // #2 triangle 0, 3, 4
+      +0.0f, +0.0f, // 0
+      +1.0f, +0.0f, +1.0f, // color 
+      +1.0f, +1.0f, // 1
+      +1.0f, +0.0f, +0.0f, // color
+      -1.0f, +1.0f, // 2 
+      +1.0f, +0.0f, +0.0f, // color
+      -1.0f, -1.0f, // 3
+      +1.0f, +0.0f, +0.0f, // color
+      +1.0f, -1.0f, // 4
+      +1.0f, +0.0f, +0.0f, // color
     };
 
   GLuint myBufferId;
   glGenBuffers(1, &myBufferId);
   glBindBuffer(GL_ARRAY_BUFFER, myBufferId);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW); 
-  glEnableVertexAttribArray(0);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
 
-  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, 0);
+
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (char*)(sizeof(float) * 2));
+}
+
+void createIndexBuffer()
+{
+  GLushort indices[] = {
+    0, 1, 2,
+    0, 3, 4
+  };
+  GLuint indexBufferID;
+  glGenBuffers(1, &indexBufferID);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+  
+}
+
+int main()
+{
+  GLFWwindow* window = windowSetUp();
+
+  createVertexBuffer();
+  createIndexBuffer();
   
   while (!glfwWindowShouldClose(window))
     {
@@ -69,8 +95,8 @@ int main()
       glClearColor(0.4f, 0.3f, 0.8f, 1.0f);
       glClear(GL_COLOR_BUFFER_BIT);
 
-      glDrawArrays(GL_TRIANGLES, 0,  6);
-
+      //glDrawArrays(GL_TRIANGLES, 0,  6);
+      glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
       glfwSwapBuffers(window);
       glfwPollEvents();
     }

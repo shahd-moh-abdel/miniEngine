1abc6e63c4cd71b4417093873e74ec79ff6954e8
shahd-moh-abdel <shahd.mohammed.abdel@gmail.com>
2025-09-02 15:43:04 +0300

    colors int the glsl

--- res/shaders/shaders.glsl
+++ res/shaders/shaders.glsl
@@ -2,18 +2,22 @@
 #version 430
 
 in layout(location = 0) vec2 position;
+in layout(location = 1) vec3 vertexColor;
+
+out vec3 theColor;
 
 void main()
 {
   gl_Position = vec4(position, 0.0, 1.0);
+  theColor = vertexColor;
 }
 
 #shader fragmentShader
 #version 430
 
 out vec4 a_color;
-
+in vec3 theColor;
 void main()
 {
-  a_color = vec4(0.0, 1.0, 0.0, 1.0); //RGBA
+  a_color = vec4(theColor, 1.0); //RGBA
 }

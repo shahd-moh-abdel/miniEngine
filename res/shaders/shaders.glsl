8e7e9d0797c35a94aae62bfb5b178ab74d8ce263
shahd-moh-abdel <shahd.mohammed.abdel@gmail.com>
2025-09-05 22:43:24 +0300

    testing glsl functions

--- res/shaders/shaders.glsl
+++ res/shaders/shaders.glsl
@@ -14,24 +14,29 @@ void main()
 
 #shader fragmentShader
 #version 430
+#define PI 3.141592653
 
 uniform vec3 iResolution;
 uniform float iTime;
 uniform vec4 iMouse;     
 out vec4 fragColor;
 
-float plot(vec2 uv)
+float plot(vec2 uv, float pct)
 {
-  return smoothstep(0.02, 0.0, abs(uv.y - uv.x));
+  return smoothstep(pct-0.02, pct, uv.y) -
+    smoothstep(pct, pct+0.02, uv.y);
 }
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord)
 {
     vec2 uv = fragCoord / iResolution.xy;
     float mp = iMouse.x / iResolution.x;
+
+    //float y = pow(uv.x, 5.0);
+    //float y = log(uv.x * PI);
     
-    uv -= .5;
-    uv.x *= iResolution.x / iResolution.y;
+    //uv -= .5;
+    //uv.x *= iResolution.x / iResolution.y;
 
     float circleRad = 0.2;
     float d = length(uv);
@@ -39,10 +44,10 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
     vec3 circleColor = vec3(abs(sin(iTime)), abs(sin(mp)), abs(cos(iTime)));
     vec3 c = vec3(1.);
     
-    if (d < circleRad) c = circleColor; else c = vec3(1.);
+    if (d < circleRad) c = circleColor; else c = vec3(y, 0.0, 0.0);
 
     // plot a line
-    float pct = plot(uv);
+    float pct = plot(uv, y);
     c = (1.0-pct)*c+pct*vec3(0.0,1.0,0.0);
     
     fragColor = vec4(c,1.0);

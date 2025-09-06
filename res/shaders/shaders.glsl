8bfb212c9c1b878b269b853abf87723d144b5fad
shahd-moh-abdel <shahd.mohammed.abdel@gmail.com>
2025-09-06 05:28:04 +0300

    mouse interactions

--- res/shaders/shaders.glsl
+++ res/shaders/shaders.glsl
@@ -26,28 +26,36 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
   //normalize
   vec2 uv = (fragCoord - 0.5 * iResolution.xy) / iResolution.xy;
 
+  vec2 mouse = iMouse.xy / iResolution.xy;
+  mouse = (mouse - 0.5) * 2.0;
+
   float t = iTime * 0.8;
   
   float angle = atan(uv.y, uv.x);
   float rad = length(uv);
 
-  float spiral = angle + rad * 3.0 + t;
+  float mouseInfluence = 1.0 / (length(uv - mouse * 0.8) + 0.1);
+
+  float spiral = angle + rad * 3.0 + t + mouseInfluence * 0.4;
   float spiralPattern = sin(spiral * 4.0) * 0.5 + 0.5;
 
   float rings = sin(rad * 15.0 - t * 3.0) * 0.5 + 0.5;
-
   rings *= smoothstep(0.0, 0.1, rad) * smoothstep(1.0, 0.6, rad);
   float combined = spiralPattern * 0.7 + rings * 0.6;
   
-  //float pulse = sin(t * 2.0) * 0.2 + 0.8;
-  //pattern *= pulse;
+  float pulse = sin(t * 2.0) * 0.2 + 0.8;
+  spiralPattern *= pulse;
 
   vec3 color1 = vec3(0.1, 0.0, 0.4);
   vec3 color2 = vec3(0.8, 0.2, 0.8);
   vec3 color3 = vec3(0.2, 0.6, 1.0);
   
   vec3 color = mix(color1, color2, spiralPattern);
-  color = mix(color, color3, rings * 0.8);
+  color = mix(color, color3, rings * 0.4);
+
+  float shimmer = sin(length(uv - mouse) * 20 + t * 6.0) * 0.1 + 0.9;
+  color *= shimmer;
+  
   fragColor = vec4(color,1.0);
 }
 
